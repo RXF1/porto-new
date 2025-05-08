d1eadaef59700370a8c4aa50242cb2ce7db1b37a
RXF1 <rafia0020@gmail.com>
2025-05-08 23:04:31 +0700

    Upload project pertama

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,18 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
+    container:{ 
+      center:true,
+      padding:'16px',
+    },
     extend: {
       colors:{ 
-        primary:'#7cfcc2',
+        primary:'#6ee7b7',
+        dark:'#0f172a',
+        secondary:'#9ca3af'
+      },
+      screens:{
+        '2xl':'1320px',
       },
     },
   },
